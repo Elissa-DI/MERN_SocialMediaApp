1f93a3eed49492302f835c5d4e65843958b72b1b
Elissa <elissadusabe@gmail.com>
2023-09-25 21:37:20 +0200

    new features

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -3,12 +3,14 @@ import LogoSearch from '../LogoSearch/LogoSearch'
 import ProfileCard from '../ProfileCard/ProfileCard'
 
 import  './ProfileSide.css'
+import FollowersCard from '../FollowersCard/FollowersCard'
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
         <LogoSearch />
         <ProfileCard />
+        <FollowersCard />
     </div>
   )
 }
