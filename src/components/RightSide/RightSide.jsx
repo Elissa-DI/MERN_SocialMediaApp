0a08284a31483ca2cf148289530f00d33809e800
Elissa <elissadusabe@gmail.com>
2023-10-02 21:37:58 +0200

    new features

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './RightSide.css'
 
+import Home from '../../img/home.png'
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+
 const RightSide = () => {
   return (
-    <div>RightSide</div>
+    <div className="RightSide">
+        <div className="navIcons">
+            <img src={Home} alt="" />
+            <UilSetting />
+            <img src={Noti} alt="" />
+            <img src={Comment} alt="" />
+        </div>          
+    </div>
   )
 }
 
