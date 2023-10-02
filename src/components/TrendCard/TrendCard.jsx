b65d9170e6a51e29a0a53f6543402e2aa1cb2bb1
Elissa <elissadusabe@gmail.com>
2023-10-02 21:50:41 +0200

    new features

--- src/components/TrendCard/TrendCard.jsx
+++ src/components/TrendCard/TrendCard.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './TrendCard.css'
 
+import { TrendData } from '../../Data/TrendData'
+
 const TrendCard = () => {
   return (
     <div className="TrendCard">
-        
+        <h3>Trends or you</h3>
+
+        {
+            TrendData.map((trend) => {
+                return(
+                    <div className='trend'>
+                        <span>#{trend.name}</span>
+                        <span>{trend.shares}</span>
+                    </div>
+                )
+            })
+        }
     </div>
   )
 }
