6dfe9c2e04ebb0539ade67fa100e21c1589dd44a
Elissa <elissadusabe@gmail.com>
2023-10-03 06:01:12 +0200

    new features

--- src/components/InfoCard/InfoCard.jsx
+++ src/components/InfoCard/InfoCard.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './InfoCard.css'
 
+import { UilPen } from '@iconscout/react-unicons'
+
 const InfoCard = () => {
   return (
-    <div>InfoCard</div>
+    <div className="InfoCard">
+        <div className="infoHead">
+            <h4>Your Info</h4>
+            <UilPen />  
+        </div>
+
+        <div className="info">
+            <span>
+                <b>Status</b>
+            </span>
+            <span>in Relationship</span>
+        </div>
+        <div className="info">
+            <span>
+                <b>Lives in</b>
+            </span>
+            <span>London</span>
+        </div>
+        <div className="info">
+            <span>
+                <b>Works at</b>
+            </span>
+            <span>MicroSoft</span>
+        </div>
+    </div>
   )
 }
 
--- src/components/ProfileLeft/ProfileLeft.jsx
+++ src/components/ProfileLeft/ProfileLeft.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './ProfileLeft.css'
 import LogoSearch from '../LogoSearch/LogoSearch'
 import FollowersCard from '../FollowersCard/FollowersCard'
+import InfoCard from '../InfoCard/InfoCard'
 
 const ProfileLeft = () => {
   return (
     <div className="ProfileLeft">
         <LogoSearch />
+        <InfoCard />
         <FollowersCard />
     </div>
   )
