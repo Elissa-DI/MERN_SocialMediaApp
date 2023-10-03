f79c75f2c6e3d4cc9265d8f6e189088b10833c52
Elissa <elissadusabe@gmail.com>
2023-10-03 07:44:51 +0200

    new features

---
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -0,0 +1,36 @@
+import { Modal, useMantineTheme } from "@mantine/core";
+
+function ProfileModal({ modalOpened, setModalOpened }) {
+  const theme = useMantineTheme();
+
+  return (
+    <Modal
+      overlayColor={
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[9]
+          : theme.colors.gray[2]
+      }
+      overlayOpacity={0.55}
+      overlayBlur={3}
+      size="55%"
+      opened={modalOpened}
+      onClose={() => setModalOpened(false)}
+    >
+      <form className="infoForm">
+        <h3>Your info</h3>
+
+        <div>
+          <input
+            type="text"
+            className="infoInput"
+            name="FirstName"
+            placeholder="First Name"
+          />
+
+
+      </form>
+    </Modal>
+  );
+}
+
+export default ProfileModal;
